695b847528a84c7cad3b012739cb1f4f7bd337dc
BartoszF <felis.bartosz@gmail.com>
2020-01-25 09:32:09 +0100

    Better platforming code

--- Assets/_SGJ2020/Scripts/PlayerController.cs
+++ Assets/_SGJ2020/Scripts/PlayerController.cs
@@ -6,9 +6,16 @@ using UnityEngine.Serialization;
 [RequireComponent(typeof(Collider2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float jumpForce = 100f;
-    public float horizontalForce = 100f;
-    
+    public Transform rayOrigin;
+    public float jumpVelocity = 5f;
+
+    public float horizontalVelocity = 10f;
+
+    public float maxSpeed = 5f;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
     private Rigidbody2D _rigidbody2D;
     private Collider2D _collider2D;
 
@@ -20,11 +27,29 @@ public class PlayerController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        var isOnFloor = Physics2D.Raycast(transform.position, Vector2.down, 0.9f);
-        if (isOnFloor.collider && _collider2D.IsTouching(new ContactFilter2D()) && Input.GetAxis("Jump") > 0)
+        Vector2 result = _rigidbody2D.velocity;
+        var isOnFloor = Physics2D.Raycast(rayOrigin.position, Vector2.down, 0.05f);
+
+        if (isOnFloor.collider /*&& _collider2D.IsTouching(new ContactFilter2D())*/ && Input.GetButton("Jump"))
         {
-            _rigidbody2D.AddForce(Vector2.up * jumpForce);            
+            result += Vector2.up * jumpVelocity;
         }
-        _rigidbody2D.AddForce(Vector2.right * (Input.GetAxis("Horizontal") * horizontalForce));
+
+        result += Vector2.right * (Input.GetAxis("Horizontal") * horizontalVelocity) * Time.fixedDeltaTime;
+
+        if (_rigidbody2D.velocity.y < 0)
+        {
+            result += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (_rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            result += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+
+        if(Math.Abs(result.x) > maxSpeed) {
+            result.x = Math.Sign(result.x) * maxSpeed;
+        }
+
+        _rigidbody2D.velocity = result;
     }
 }
