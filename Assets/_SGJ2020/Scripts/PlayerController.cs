25685cef315f897d11a921e8442e73556e85b2af
BartoszF <felis.bartosz@gmail.com>
2020-01-25 11:36:45 +0100

    Primitive wall jump

--- Assets/_SGJ2020/Scripts/PlayerController.cs
+++ Assets/_SGJ2020/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ using UnityEngine.Serialization;
 [RequireComponent(typeof(Collider2D))]
 public class PlayerController : MonoBehaviour
 {
-    public Transform rayOrigin;
+    public Transform rayDownOrigin;
+
+    public Transform rayLeftOrigin, rayRightOrigin;
     public float jumpVelocity = 5f;
+    public Vector2 wallJumpVelocity = new Vector2(5,5);
 
     public float horizontalVelocity = 10f;
 
@@ -28,27 +31,45 @@ public class PlayerController : MonoBehaviour
     public void FixedUpdate()
     {
         Vector2 result = _rigidbody2D.velocity;
-        var isOnFloor = Physics2D.Raycast(rayOrigin.position, Vector2.down, 0.05f);
+        var isOnFloor = Physics2D.Raycast(rayDownOrigin.position, -transform.up, 0.05f);
 
-        if (isOnFloor.collider /*&& _collider2D.IsTouching(new ContactFilter2D())*/ && Input.GetButton("Jump"))
+        var isOnLeftWall = Physics2D.Raycast(rayLeftOrigin.position, -transform.right, 0.1f);
+        var isOnRightWall = Physics2D.Raycast(rayRightOrigin.position, transform.right, 0.1f);
+
+
+        if (isOnFloor.collider && Input.GetButton("Jump"))
         {
-            result += Vector2.up * jumpVelocity;
+            result += (Vector2)transform.up * jumpVelocity;
+        }
+
+        if (isOnLeftWall.collider && Input.GetButton("Jump"))
+        {
+            Vector2 direction = new Vector2();
+            direction += (Vector2)transform.right * wallJumpVelocity.x;
+            direction += (Vector2)transform.up *wallJumpVelocity.y;
+            result += direction;
+        }
+        if (isOnRightWall.collider && Input.GetButton("Jump"))
+        {
+            Vector2 direction = new Vector2();
+            direction += -(Vector2)transform.right * wallJumpVelocity.x;
+            direction += (Vector2)transform.up *wallJumpVelocity.y;
         }
 
         if(Math.Abs(Input.GetAxis("Horizontal")) > 0.1f) {
-            result += Vector2.right * (Input.GetAxis("Horizontal") * horizontalVelocity) * Time.fixedDeltaTime;
+            result += (Vector2)transform.right * (Input.GetAxis("Horizontal") * horizontalVelocity) * Time.fixedDeltaTime;
         } else {
-            result.x *= 0.3f * Time.fixedDeltaTime;
+            //result.x *= 0.3f * Time.fixedDeltaTime;
         }
 
 
         if (_rigidbody2D.velocity.y < 0)
         {
-            result += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+            result += (Vector2)transform.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
         }
         else if (_rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
         {
-            result += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+            result += (Vector2)transform.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
         }
 
         if(Math.Abs(result.x) > maxSpeed) {
